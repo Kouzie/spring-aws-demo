8eb085284a6026e1b7a552491c804df9eb327f41
Giyong Ko <kgy1996@naver.com>
2024-11-28 19:04:35 +0900

    localstack 추가

--- dynamodb-demo/src/main/kotlin/com/aws/demo/dynamodb/config/DynamodbConfig.kt
+++ dynamodb-demo/src/main/kotlin/com/aws/demo/dynamodb/config/DynamodbConfig.kt
@@ -2,8 +2,7 @@ package com.aws.demo.dynamodb.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
@@ -12,15 +11,11 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 class DynamodbConfig {
 
     @Bean
-    fun dynamoDbClient(): DynamoDbClient {
+    fun dynamoDbClient(credentialsProvider: AwsCredentialsProvider): DynamoDbClient {
         return DynamoDbClient.builder()
-            .endpointOverride(java.net.URI("http://localhost:8000")) // 로컬 DynamoDB URL
+            .endpointOverride(java.net.URI("http://localhost:4566")) // 로컬 DynamoDB URL
             .region(Region.US_EAST_1)
-            .credentialsProvider(
-                StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create("fakeAccessKey", "fakeSecretKey") // 로컬 DynamoDB는 임의의 키 사용
-                )
-            )
+            .credentialsProvider(credentialsProvider)
             .build()
     }
 
